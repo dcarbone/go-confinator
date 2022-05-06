c8afa6ccb91ccc3d374e78a257e2cb02b60f1101
Daniel Carbone <daniel.carbone@ironnetcybersecurity.com>
2022-05-06 10:06:07 -0500

    more efficient init (#1)
    - using nil typing for default func map building
    - small update to test suite
    - adding nil var check

--- confinator.go
+++ confinator.go
@@ -3,7 +3,6 @@ package confinator
 import (
 	"flag"
 	"fmt"
-	"reflect"
 	"sync"
 )
 
@@ -21,18 +20,17 @@ func NewConfinator() *Confinator {
 func (cf *Confinator) RegisterFlagVarType(varPtr interface{}, fn FlagVarTypeHandlerFunc) {
 	cf.mu.Lock()
 	defer cf.mu.Unlock()
-	varType := reflect.TypeOf(varPtr)
-	if varType.Kind() != reflect.Ptr {
-		panic(fmt.Sprintf("Must provided a pointer, saw %T", varPtr))
-	}
-	cf.types[buildFlagVarTypeKey(varType)] = fn
+	cf.types[kfn(varPtr)] = fn
 }
 
 // FlagVar is a convenience method that handles a few common config struct -> flag cases
 func (cf *Confinator) FlagVar(fs *flag.FlagSet, varPtr interface{}, name, usage string) {
 	cf.mu.RLock()
 	defer cf.mu.RUnlock()
-	if fn, ok := cf.types[buildFlagVarTypeKey(reflect.TypeOf(varPtr))]; ok {
+	if varPtr == nil {
+		panic("varPtr cannot be nil")
+	}
+	if fn, ok := cf.types[kfn(varPtr)]; ok {
 		fn(fs, varPtr, name, usage)
 	} else {
 		panic(fmt.Sprintf("invalid type: %T", varPtr))
